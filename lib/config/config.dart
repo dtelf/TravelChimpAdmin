55ebfe0a547fdef1c314df014ff080192c5e046c
Ted <theo.caleb.lewis@gmail.com>
2022-07-06 02:03:00 +1000

    First commit

---
+++ lib/config/config.dart
@@ -0,0 +1,9 @@
+
+class Config{
+  final String appName = 'TravelChimp'; //app name
+  final String testerPassword = 'tester1234';  //testing password - don't use this password in the database (you can change this testing password too)
+
+  //firebase server token for push notication
+  final String serverToken = 'BDakbRmMBrhoQ2vKBWzt6mY1y-e3TPb5lcu98otFL0qKYaKojnpTor20DYz5aUZ8eTPzt5b4pMXmEqEhqY5PXqk';
+  final String icon = 'assets/images/icon.png'; // app icon
+}
